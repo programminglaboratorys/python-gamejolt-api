760187bbd478c4fe67377f3a87d53f93e26e02f7
programminglaboratorys <programming.laboratorys@gmail.com>
2024-12-17 16:59:51 +0300

    documentation for subcomponents __init__

--- src/gamejolt/subcomponents/__init__.py
+++ src/gamejolt/subcomponents/__init__.py
@@ -1,3 +1,10 @@
+"""
+This subcomponents package provides various components for interacting with the Game Jolt API.
+
+every endpoint has it's own subcomponent
+example: `UsersComponent` for `/users/` which is responsible for handling user-related operations.
+"""
+
 from .users import UsersComponent
 
 __all__ = ["UsersComponent"]
